e4dfc059bec7979cf2a6eb08cb6ca56fd2e36b07
jusimargv <jusimargv@gmail.com>
2024-05-22 20:10:49 -0300

    Doc_update

---
+++ javascript/variaveis_constantes.js
@@ -0,0 +1,13 @@
+let roupa = 'camiseta';
+roupa = 'moletom';
+console.log(roupa);
+//console.log(roupa); //moletom
+
+let comidaFavorita = 'Pizza';
+console.log(comidaFavorita);
+comidaFavorita="macarrão";
+console.log(comidaFavorita);
+
+let marca ='Toyota';
+let carro ='Camry';
+console.log(marca +" "+ carro);
